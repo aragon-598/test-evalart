31a8d304cb09312c477b9228859277d83c581e13
Alejandro Aragon <aragondru155@gmail.com>
2023-08-27 14:30:05 -0600

    optimizing code & changing logic

--- src/App.java
+++ src/App.java
@@ -2,16 +2,12 @@ public class App {
     public static void main(String[] args){
 
         int[] myArray = {13,2,4,35,1};
-        for (int i = myArray.length-1; i >0 ; i--) {
-            for (int j = 0; j < i; j++) {
-                if (myArray[j]>myArray[j+1]) {
-                    int value = myArray[j+1];
-                    myArray[j+1] = myArray[j];
-                    myArray[j] = value;
-                }
-            }
+        
+        int max = 0;
+        for (int i = 0; i < myArray.length; i++) {
+            if(myArray[i]>max)
+                max = myArray[i];
         }
-            int maxNumber = myArray[myArray.length-1];
-            System.out.println(maxNumber);
+        System.out.println(max);
     }
-}
+    }
